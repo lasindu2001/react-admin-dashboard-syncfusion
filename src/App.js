b4b37555e8f4ef3262b1e3f775607f5bbc77a99a
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-26 01:40:47 +0530

    minor changes to theme

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ import './App.css'
 const App = () => {
   const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
 
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+  
   return (
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
